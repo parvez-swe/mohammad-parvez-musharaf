24591e73a6ee192793f652163eb7961c15bcf258
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-09-25 02:22:18 +0600

    navbar customization

--- components/NavBar/Navbar.js
+++ components/NavBar/Navbar.js
@@ -1,32 +1,31 @@
 import React, { useState, useRef, useEffect } from "react";
 import { links } from "./data";
 import Image from "next/image";
-import myLOader from './images/profile.jpg'
+import myLOader from "./images/profile.jpg";
 const Navbar = () => {
   return (
     <>
-      <div className="flex flex-col md:flex-row bg-red-400 w-[80%] md:w-[100%] h-[100vh] md:h-12 md:justify-between ">
-        <div className="pl-5 md:ml-12 flex flex-col md:flex-row h-[30%] bg-slate-300">
-          <div className="pt-5">
+      <div className="flex flex-col md:flex-row bg-red-400 w-[80%] md:w-[100%] h-[100vh] md:h-16 md:justify-between ">
+        <div className="pl-5 flex flex-col md:flex-row h-[32%] md:h-12 bg-slate-300 md:bg-red-400">
+          <div className="pt-5 md:pt-0  h-10 w-12">
             {" "}
             <Image
-            className="rounded-full"
-              
+              className="rounded-[100%] h-8 w-12"
               src={myLOader}
               alt="Picture of the author"
-              width={80}
-              height={80}
             />
           </div>
-          <div>Name</div>
-          <div>email</div>
+          <div className="mt-16 md:mt-0 md:ml-2">
+            <h3 className="font-bold md:text-2xl text-white">Mohammad Parvez Musharaf</h3>
+            <p className="font-light md:text-semibold text-white">mdparvezmusharaf2@gmail.com</p>
+          </div>
         </div>
 
-        <ul className="ml-5 md:mr-12 uppercase flex flex-col md:flex-row ">
+        <ul className="md:mr-12 uppercase flex flex-col md:flex-row md:items-center ">
           {links.map((link) => {
             // const { id, url, text } = link;
             return (
-              <li className="py-2 px-1 hover:bg-red-300">
+              <li className="font-semibold pl-5 py-5 px-1 text-red-100 hover:text-white hover:cursor-pointer duration-700 ease-in-out">
                 <a>{link.text}</a>
               </li>
             );
